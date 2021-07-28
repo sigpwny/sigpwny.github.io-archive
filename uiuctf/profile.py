f9c5c6e243d6f16c66686a603b50bc96b2dc1ebb
Ian Klatzco <iklatzco@gmail.com>
2021-07-28 15:35:36 -0700

    add artists

--- uiuctf/profile.py
+++ uiuctf/profile.py
@@ -9,6 +9,14 @@ import os
 profiles = open('profile.txt', 'r').readlines()
 
 print("<!-- BEGIN PROFILES -->")
+print('''
+      <div class="my-5"></div><br>
+      <div class="header flicker-9">UIUCTF TEAM</div>
+      <div class="gradient my-3"></div>
+
+      <div class="row d-flex justify-content-center">
+''')
+
 # ordered by name
 for line in sorted(profiles, key=lambda x: x.split(',')[1].lower()):
   title, name, link, picture = map(lambda x : x.strip(), line.split(','))
@@ -22,6 +30,50 @@ for line in sorted(profiles, key=lambda x: x.split(',')[1].lower()):
         <h5 class="mx-3 small-text">{name.replace(" ", "<br/>")}</h5>
     </a>
   </div>''')
+print('</div>')
 print("<!--   END PROFILES -->")
+
+print("<!-- BEGIN ARTISTS -->")
+print('''
+<div class="header flicker-9">ARTISTS &amp; ASSETS</div>
+<div class="gradient my-3"></div>
+<div class="row d-flex justify-content-center">
+
+  <!-- pomegranate.boy -->
+  <div class="card m-2 tooltip-big-text" data-balloon-length="medium" aria-label="background image for the splash page" data-balloon-pos="up">
+    <a href="https://linktr.ee/pomegranate.boy">
+        <img src="profiles/pomie.png" class="rounded-circle my-3 team-image" height="120" width="120"/>
+        <h5 class="mx-3 small-text">pomegranate.boy</h5>
+    </a>
+  </div>
+
+  <!-- pluto -->
+  <div class="card m-2 tooltip-big-text" data-balloon-length="medium" aria-label="UIUCTF 2021 logo" data-balloon-pos="up">
+    <a href="https://www.facebook.com/anastasiya.peskova">
+        <img src="profiles/pluto.png" class="rounded-circle my-3 team-image" height="120" width="120"/>
+        <h5 class="mx-3 small-text">Pluto</h5>
+    </a>
+  </div>
+
+  <!-- halfprism -->
+  <div class="card m-2 tooltip-big-text" data-balloon-length="medium" aria-label="poster / possible hoodie design" data-balloon-pos="up">
+    <a href="https://twitter.com/halfprism_">
+        <img src="profiles/halfprism.jpg" class="rounded-circle my-3 team-image" height="120" width="120"/>
+        <h5 class="mx-3 small-text">halfprism</h5>
+    </a>
+  </div>
+
+  <!-- graywalf -->
+  <div class="card m-2 tooltip-big-text" data-balloon-length="medium" aria-label="The Best Challenge >:3" data-balloon-pos="up">
+    <a href="https://www.youtube.com/c/graywalf">
+        <img src="profiles/graywalf.jpg" class="rounded-circle my-3 team-image" height="120" width="120"/>
+        <h5 class="mx-3 small-text">GrayWalf</h5>
+    </a>
+  </div>
+
+''')
+
+print("</div>")
+print("<!--   END ARTISTS -->")
 
 # os.system(f'mogrify -resize 250x250 -quality 35 -gravity Center profiles/*')
