b99ecc551340e02f0eca922f00e8dd8fd16af33c
Ian Klatzco <iklatzco@gmail.com>
2021-07-28 13:05:43 -0700

    attempt to rm the downloading part

--- uiuctf/profile.py
+++ uiuctf/profile.py
@@ -1,10 +1,10 @@
 import os
 
 # reads profile.txt
-# fetches pfps from internet
+# COMMENTED OUT fetches pfps from internet
 # stores in dir
 # codegens html and prints it to stdout
-# resizes pictures
+# COMMENTED OUT resizes pictures
 
 profiles = open('profile.txt', 'r').readlines()
 
@@ -12,7 +12,7 @@ for line in sorted(profiles, key=lambda x: x.split(',')[1].lower()):
   title, name, link, picture = map(lambda x : x.strip(), line.split(','))
   curl_output = f'profiles/{name.lower().replace(" ", "_")}{os.path.splitext(picture)[1]}'
   # Surpress stdout cause I want to pipe stdout to clipboard and paste it into index.html
-  os.system(f'wget {picture} -O {curl_output} 1>/dev/null')
+  # os.system(f'wget {picture} -O {curl_output} 1>/dev/null')
   print(f'<div class="card m-2 tooltip-big-text" data-balloon-length="medium" aria-label="{title}" data-balloon-pos="up"><a href="{link}"><img src="{curl_output}" class="rounded-circle my-3 team-image" height="120" width="120"/><h5 class="mx-3 small-text">{name.replace(" ", "<br/>")}</h5></a></div>', end = '')
 
-os.system(f'mogrify -resize 250x250 -quality 35 -gravity Center profiles/*')
+# os.system(f'mogrify -resize 250x250 -quality 35 -gravity Center profiles/*')
